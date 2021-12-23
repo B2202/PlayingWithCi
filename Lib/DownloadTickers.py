1a5b421cdf1ce116c860220e6a6e040464a4d513
B2202 <sjv33@gmx.net>
2021-12-23 17:57:23 +0100

    first Download Class and test (#1)

---
+++ Lib/DownloadTickers.py
@@ -0,0 +1,13 @@
+import yfinance as yf
+import datetime
+
+class TickerUpdater:
+    def __init__(self):
+        self.globalStart = "2000-01-01"
+        self.tickerList = ['MRK']
+
+    def DownloadTickers(self):
+        newData = yf.download(self.tickerList, start=self.globalStart,
+                               stop=datetime.datetime.today(),
+                               group_by='Ticker')
+        return newData
