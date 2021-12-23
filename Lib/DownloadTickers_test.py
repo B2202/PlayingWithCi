799a72756e3717926f954836f0048672ef5156f6
MrMaxMustermann <sjv33@gmx.net>
2021-12-23 18:15:18 +0100

    Setup pytest

--- Lib/DownloadTickers_test.py
+++ Lib/DownloadTickers_test.py
@@ -6,6 +6,6 @@ def test_CorrectTypeOfTickerList():
     updateTickersInstance = TickerUpdater()
     assert isinstance(updateTickersInstance.tickerList, list)
 
-def test_CorrectTypeOfGlobalStart():
-    updateTickersInstance = TickerUpdater()
-    assert isinstance(updateTickersInstance.DownloadTickers(), pandas.DataFrame)
+#def test_CorrectTypeOfTickerReturn():
+#    updateTickersInstance = TickerUpdater()
+#    assert isinstance(updateTickersInstance.DownloadTickers(), pandas.DataFrame)
